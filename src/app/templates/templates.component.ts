39f0d2e22916248ba614e44a4f5c98771e81b489
Shiva <sivaneshkumar24@gmail.com>
2023-12-19 18:17:54 +0530

    Template page

--- src/app/templates/templates.component.ts
+++ src/app/templates/templates.component.ts
@@ -19,6 +19,7 @@ export class TemplatesComponent implements OnInit
     
   }
 
+  id:any={};
   dishes: any={};
   mainDishArray11: any[]=[];
   SideDishArray: any[]=[];
@@ -33,16 +34,56 @@ export class TemplatesComponent implements OnInit
 
 // mahi usecase
   dishSelect:any;
-
-
-
-  dishSelect1:any;
-  interNationSelect:any;
+  sidedishSelect:any;
+  interNationSelect: any[] = [];
   NationSelect:any;
-
+  adminId:any;
+  dishSelect1:any;
+  dishSelect2:any;
+  dishSelect3:any;
+  dishSelect4:any;
+  dishSelect5:any;
+  dishSelect6:any;
+  dishSelect7:any;
+  dishSelect8:any;
+  dishSelect9:any;
+  dishSelect10:any;
+  dishSelect11:any;
+  dishSelect12:any;
+  dishSelect13:any;
+  dishSelect14:any;
+  dishSelect15:any;
+  dishSelect16:any;
+  dishSelect17:any;
+  dishSelect18:any;
+  dishSelect19:any;
+  dishSelect20:any;
+  sidedishSelect1:any;
+  sidedishSelect2:any;
+  sidedishSelect3:any;
+  sidedishSelect4:any;
+  sidedishSelect5:any;
+  sidedishSelect6:any;
+  sidedishSelect7:any;
+  sidedishSelect8:any;
+  sidedishSelect9:any;
+  sidedishSelect10:any;
+  sidedishSelect11:any;
+  sidedishSelect12:any;
+  sidedishSelect13:any;
+  sidedishSelect14:any;
+  sidedishSelect15:any;
+  sidedishSelect16:any;
+  sidedishSelect17:any;
+  sidedishSelect18:any;
+  sidedishSelect19:any;
+  sidedishSelect20:any;
 
   ngOnInit() {
 
+    if (localStorage.getItem("id")) {
+      this.id = JSON.parse(localStorage.getItem("id") || '{}');
+    }
     this.GetInfo();
 
   }
@@ -78,23 +119,129 @@ this.database.getData('ypc-admin-micro-service/admin/national/cuisine').subscrib
 );
 
 
-
-
-// get national cuisne
-// this.database.getData('ypc-admin-micro-service/ypc/admin/chef/allvideos').subscribe(
-//   (result: any) => {
-
-//     this.nationalCuisine = result;
-
-// console.log( this.nationalCuisine);
-
-//   },
-// );
-
-
-
-
   } 
+
+  createTemplate(): void {
+
+
+    console.log(this.interNationSelect);
+    console.log(this.NationSelect);
+    
+      const data = {
+
+        adminId:this.id,
+        internationalCuisine: [this.interNationSelect],
+        nationalCuisine: [this.NationSelect],
+        foodPreference:this.foodPreference,
+        chart:
+    [
+            {
+            "Monday":{
+                        "maindish": [
+                                        {"breakfast":this.dishSelect,"lunch":this.dishSelect1,"dinner":this.dishSelect2}
+                        ],
+                        "sidedish":[
+                                        {"breakfast":this.sidedishSelect,"lunch":this.sidedishSelect1,"dinner":this.sidedishSelect2}
+                        ]
+                    },
+                        
+        
+            }     ,
+            {
+              "Tuesday":{
+                          "maindish": [
+                                          {"breakfast":this.dishSelect3,"lunch":this.dishSelect4,"dinner":this.dishSelect5}
+                          ],
+                          "sidedish":[
+                                          {"breakfast":this.sidedishSelect3,"lunch":this.sidedishSelect4,"dinner":this.sidedishSelect5}
+                          ]
+                      },
+                          
+          
+              }    ,
+              {
+                "Wednesday":{
+                            "maindish": [
+                                            {"breakfast":this.dishSelect6,"lunch":this.dishSelect7,"dinner":this.dishSelect8}
+                            ],
+                            "sidedish":[
+                                            {"breakfast":this.sidedishSelect6,"lunch":this.sidedishSelect7,"dinner":this.sidedishSelect8}
+                            ]
+                        },
+                            
+            
+                }    ,
+                {
+                  "Thursday":{
+                              "maindish": [
+                                              {"breakfast":this.dishSelect9,"lunch":this.dishSelect10,"dinner":this.dishSelect11}
+                              ],
+                              "sidedish":[
+                                              {"breakfast":this.sidedishSelect9,"lunch":this.sidedishSelect10,"dinner":this.sidedishSelect11}
+                              ]
+                          },
+                              
+              
+                  }   , 
+                  {
+                    "Friday":{
+                                "maindish": [
+                                                {"breakfast":this.dishSelect12,"lunch":this.dishSelect13,"dinner":this.dishSelect14}
+                                ],
+                                "sidedish":[
+                                                {"breakfast":this.sidedishSelect12,"lunch":this.sidedishSelect13,"dinner":this.sidedishSelect14}
+                                ]
+                            },
+                                
+                
+                    }    ,
+                    {
+                      "Saturday":{
+                                  "maindish": [
+                                                  {"breakfast":this.dishSelect15,"lunch":this.dishSelect16,"dinner":this.dishSelect17}
+                                  ],
+                                  "sidedish":[
+                                                  {"breakfast":this.sidedishSelect15,"lunch":this.sidedishSelect16,"dinner":this.sidedishSelect17}
+                                  ]
+                              },
+                                  
+                  
+                      }    ,
+                      {
+                        "Sunday":{
+                                    "maindish": [
+                                                    {"breakfast":this.dishSelect18,"lunch":this.dishSelect19,"dinner":this.dishSelect20}
+                                    ],
+                                    "sidedish":[
+                                                    {"breakfast":this.sidedishSelect18,"lunch":this.sidedishSelect19,"dinner":this.sidedishSelect20}
+                                    ]
+                                },
+                                    
+                    
+                        }    
+
+
+    ]
+    
+      };
+  console.log(data);
+  
+      this.database.postdata(`ypc-admin-micro-service/admin/template/1`, data).subscribe({
+        next: (result) => {
+          console.log(result);
+          // this.result = result.message;
+          // this.succesMsg(this.result);
+        },
+        error: (error) => {
+          // console.log(error);
+          // this.errorMsg(error.error.error);
+        },
+        complete: () => {
+     console.log("completed ..........");
+     
+        }
+      });
+    }
 
 
   onItemSelect(item: any) {
@@ -114,7 +261,7 @@ this.database.getData('ypc-admin-micro-service/admin/national/cuisine').subscrib
   
      
   
-      this.database.getData('ypc-admin-micro-service/admin/template/getProducts/2'+food).subscribe(
+      this.database.getData('ypc-admin-micro-service/admin/template/getProducts/1'+food).subscribe(
         (result: any={}) => 
       {
         
@@ -148,4 +295,8 @@ this.database.getData('ypc-admin-micro-service/admin/national/cuisine').subscrib
     throw new Error('Method not implemented.');
     }
   
+
+
+
+
 }
